bf6f21b5c9e2b80d95e5db888a281eb17c4f1951
lucian lin <lucian_lin@cmoney.com.tw>
2023-05-29 14:03:15 +0800

    feat: add sitemap setting

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vitepress'
 import vuetify from 'vite-plugin-vuetify'
+import { generateSitemap } from 'sitemap-ts'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
@@ -8,6 +9,14 @@ export default defineConfig({
   description: "Record my life in this, and share my programming experience.",
   lastUpdated: true,
   themeConfig: {
+    search: {
+      provider: 'algolia',
+      options: {
+        appId: '7ZAAM6ULLY',
+        apiKey: 'f8ca11213e8877297fa005f560eb6f8a',
+        indexName: 'blackfloat'
+      }
+    },
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
@@ -28,5 +37,11 @@ export default defineConfig({
       // TODO: workaround until they support native ESM
       noExternal: ['vuetify'],
     },
+  },
+  buildEnd({ outDir }) {
+    generateSitemap({
+      hostname: 'https://blackfloat.club/',
+      outDir,
+    })
   },
 })
